e9053ec335dad990a3f6f54c0a60216160b88018
Behnoosh Mohammadzadeh <behnooshmzd@gmail.com>
2022-11-05 23:35:05 +0330

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,327 @@
 var font;
 var vehicles = [];
-var n = 1;
+var n = 0;
 var radius = 3;
 var w = 0;
 var counter = 0;
 var h;
 var lnheight;
+var sw = 0;
+var textcolor = 0;
+var textcolor1 = 0;
+var textcolor2 = 0;
+var textcolor3 = 255;
+var timer = 10000.0;
+var time = 0.0;
+var dt = 0.95;
+let fa;
+let offset = 0;
+let easing = 0.05;
+
+let poly = []
+let poly2 = []
+let num = 100, num2 = 100;
+var background1;
+let img;
 
 function preload() {
   font = loadFont('AvenirNextLTPro-Demi.otf');
+  fa = loadFont('fa.otf');
+  br = loadFont('br.otf');
+  background1 = loadImage("back2.gif");
+  img = loadImage('beh.jpg');
 }
-
 function setup() {
-  createCanvas(windowWidth-20, windowHeight-20);
-  background(51);
-  var points = font.textToPoints('dualsyncopath', windowWidth/9, windowHeight/2, windowWidth/9, {
-    sampleFactor: 0.25
-  });
-
-  h = height/(height/2.1);
+  createCanvas(windowWidth - 23, windowHeight - 22);
+  smooth()
+  if (width > 500) {
+    var points = font.textToPoints('Dualsyncopath', windowWidth / 3.9, windowHeight / 2, windowWidth / 15, {
+      sampleFactor: 0.25
+    });
+  } else {
+    var points = font.textToPoints('Dualsyncopath', windowWidth / 10, windowHeight / 2, windowWidth / 9, {
+      sampleFactor: 0.25
+    });
+  }
+  h = height / (height / 2.1);
   for (var i = 0; i < points.length; i++) {
     var pt = points[i];
     var vehicle = new Vehicle(pt.x, pt.y);
     vehicles.push(vehicle);
   }
+
 }
 
 function draw() {
-  background(51);
+  background(0, 2, 7)
+  clear()
+  if (n >= 2) {
+    if (textcolor3 >= 0) {
+      textcolor3 -= 0.9;
+    }
+  } else {
+    if (textcolor3 < 255) {
+      textcolor3 += 10;
+    }
+  }
+
+  image(background1, 0, 0, width, height);
+  stroke(0, 2, 7, textcolor3)
+  fill(0, 2, 7, textcolor3)
+  rect(0, 0, width, height)
+
+  // var color2 = color(255, 120, 226);
+  // var color1 = color(255, 196, 120);
+  // push()
+  // blendMode(OVERLAY)
+  // for (var x = 0; x < width; x++)
+  // {
+  //   stroke(0, color1, color2);
+  //   line(x, 0, x, height);
+  //   color1 = color1 - 0.25
+  //   color2 += 0.25;
+  // }
+  // pop()
+
+  if (width > 500) {
+    for (var j = 0; j < 4; j++) {
+      if (n == j) {
+        fill(250, 80);
+      } else {
+        fill(250, 0);
+      }
+      stroke(250, 50);
+      ellipse(width - 30, height / 2.4 + (j * 30), 10, 10);
+    }
+  }
+
+  if (width >= 1500) {
+    textSize(18);
+  } else if (width >= 1900) {
+    textSize(20);
+  } else {
+    textSize(14)
+  }
+
+  if (n == 1) {
+    if (textcolor1 < 255) {
+      textcolor1 += 5;
+    }
+  } else {
+    if (textcolor1 >= 0) {
+      textcolor1 -= 10;
+    }
+  }
+  if (textcolor1 >= 0) {
+    push();
+    rectMode(CENTER)
+    textAlign(CENTER, CENTER)
+    noStroke();
+    fill(textcolor1)
+    textWithBlur("~ Only those who attempt the absurd will achieve the impossible. ~", height - 130, textcolor1, width / 20, 13)
+    textWithBlur("- M. C. Escher", height - 100, textcolor1, width / 20 + 10, 13)
+    pop();
+  }
+
+  if (n == 2) {
+    if (textcolor <= 50) {
+      textcolor += 1;
+    } else if (textcolor > 50 & textcolor < 255) {
+      textcolor += 3;
+    }
+  } else {
+    if (textcolor >= 0) {
+      textcolor -= 10;
+    }
+  }
+  if (textcolor >= 0) {
+    push();
+    rectMode(CENTER)
+    textAlign(CENTER, CENTER)
+    noStroke();
+    fill(textcolor)
+    textWithBlur("Dualsyncopath", 100 * 3 + 25, textcolor, width / 20 + 5, 25)
+    textWithBlur("is an invitation to those seeking ", 100 * 3 + 60, textcolor, width / 20, 13)
+    textWithBlur("multidisciplinary collaboration", (100 * 3 + 60) + 30, textcolor, width / 20 + 2, 13)
+    textWithBlur("in creative applications.", (100 * 3 + 60) + 60, textcolor, width / 20, 13)
+    // textWithBlur("in computer science.", (100 * 3 + 60) + 90, textcolor, width / 20, 13)
+    pop();
+  }
+
+  if (n == 3) {
+    if (textcolor2 <= 50) {
+      textcolor2 += 1;
+    } else if (textcolor2 > 50 & textcolor2 < 255) {
+      textcolor2 += 3;
+    }
+  } else {
+    if (textcolor2 >= 0) {
+      textcolor2 -= 10;
+    }
+  }
+ 
+  
+  if (textcolor2 >= 0) {
+    push();
+    rectMode(CENTER)
+    textAlign(CENTER, CENTER)
+    noStroke();
+    fill(textcolor2)
+    tint(255, textcolor2); // Display at half opacity
+    image(img, width/20, 100 + 25 );
+    textWithBlur("Who Am I?", 100 * 3 + 25, textcolor2, width / 20 + 5 , 25)
+    textWithBlur("I’m Behnoosh Mohammadzadeh, a creative coder", 100 * 3 + 60, textcolor2, width / 20 , 13)
+    textWithBlur("and Ph.D. student in Human-Computer Interaction", (100 * 3 + 60) + 30, textcolor2, width / 20 + 2, 13)
+    textWithBlur("working on Collaborative Machine Teaching.", (100 * 3 + 60) + 60, textcolor2, width / 20, 13)
+    textWithBlur("I’m interested in conducting collaborative ", (100 * 3 + 60) + 90, textcolor, width / 20, 13)
+    textWithBlur("scenarios in Interactive Machine Learning among", (100 * 3 + 60) + 120, textcolor, width / 20, 13)
+    textWithBlur("domain experts in artistic fields.", (100 * 3 + 60) + 150, textcolor, width / 20, 13)
+    textWithBlur("Looking forward to your collective ideas!", (100 * 3 + 60) + 180, textcolor, width / 20, 13)
+    pop();
+  }
+
+
+  push();
   for (var i = 0; i < vehicles.length; i++) {
-    if(n == 0){
-      radius = 6 * sin(i / 40 + frameCount / 30);
-      if(counter<=160){
-      counter += 0.001*random();
-      }else{
+    if (n == 2) {
+      radius = 3.5 * sin(i / 60 + frameCount / 100);
+      if (counter >= 0) {
+        counter -= 0.002;
+      } else {
+        counter = 0;
+      }
+    } else if (n == 3) {
+      if (i % 2 == 1) {
+        radius = 8 * sin(i / 40 + frameCount / 30);
+      } else {
+        radius = 2 * sin(i / 40 + frameCount / 30);
+      }
+      if (counter <= 160) {
+        counter += 0.001 * random();
+      } else {
         counter = 160;
       }
-    }else{
-      radius = 3 * sin(i / 60 + frameCount / 30);
-      if(counter >= 0){
+    } else {
+      radius = 2 * sin(i / 60 + frameCount / 100);
+      if (counter >= 0) {
         counter -= 0.002;
-      }else{
+      } else {
         counter = 0;
       }
     }
     var v = vehicles[i];
-    var ln = ((i%10)*10*ceil(i/10));
-    var lm = 80*sin(radians(ln-w));
-    lnheight = ln*h;
-    v.behaviors(n,1,width/2+lm, lnheight, width/2-lm, lnheight+15);
+    var ln = ((i % 10) * 10 * ceil(i / 10));
+    var lm = 80 * sin(radians(ln - w));
+    if (ln * h < height + 500) {
+      lnheight = ln * h;
+    } else {
+      if (i % 2 == 0) {
+        lnheight = -100;
+      } else {
+        lnheight = height + 100;
+      }
+    }
+    if (i % 2 == 0) {
+      sw = 1;
+    } else {
+      sw = 0;
+    }
+    v.behaviors(n, sw, width / 2 + lm, lnheight, width / 2 - lm, lnheight + 15, random(width), random(height), (sin(i / 60 + frameCount / 100)), dt);
     v.update();
-    v.show(radius);
+    v.show(radius, n, sw);
     v.spin(counter);
   }
   w++;
+  pop();
+}
+
+function keyPressed() {
+  clear()
+  if (keyCode === LEFT_ARROW) {
+    if (n > 0) {
+      n -= 1;
+      clear()
+    }
+    if (n < 2) {
+      time = 0;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    if (n < 3) {
+      n += 1;
+      clear()
+    }
+  } else if (keyCode === UP_ARROW) {
+    if (n > 0) {
+      n -= 1;
+      clear()
+    }
+    if (n < 2) {
+      time = 0;
+    }
+  } else if (keyCode === DOWN_ARROW) {
+    if (n < 3) {
+      n += 1;
+      clear()
+    }
+  }
+}
+
+function mouseWheel(event) {
+  clear()
+  if (event.delta > 0) {
+    if (n < 3) {
+      n += 1;
+      clear()
+    }
+  } else {
+    if (n > 0) {
+      clear()
+      n -= 1;
+    }
+    if (n < 2) {
+      time = 0;
+    }
+  }
 }
 
 
-function mouseWheel(event) {
-  if(event.delta>0){
-    n = 0;
-  }else{
-    n = 1;
+function textWithBlur(tmp, thisHeight, textcolor, space, textsize) {
+  currentHeight = thisHeight;
+  push()
+  offset = space
+  drawingContext.shadowBlur = 30
+  drawingContext.shadowColor = color(textcolor)
+  textSize(textsize)
+  for (var i = 0; i < tmp.length; i++) {
+    thisChar = tmp[i]
+    text(thisChar, offset, currentHeight)
+    offset += textsize / 1.2
   }
+  pop()
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+  if (axis == "Y") { // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      var inter = map(i, y, y + h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis == "X") { // Left to right gradient
+    for (let j = x; j <= x + w; j++) {
+      var inter2 = map(j, x, x + w, 0, 1);
+      var d = lerpColor(c1, c2, inter2);
+      stroke(d);
+      line(j, y, j, y + h);
+    }
+  }
+
 }
--- vehicle.js
+++ vehicle.js
@@ -5,24 +5,52 @@ function Vehicle(x, y) {
   this.acc = createVector();
   this.maxspeed = 15;
   this.maxforce = 1;
-  this.tar = createVector(0,0);
+  this.tar = createVector(0, 0);
   this.arrivemult = 1;
   this.fleemult = 5;
 }
 
-Vehicle.prototype.behaviors = function(turn,sw,dna_x, dna_y, dnaTwin_x, dnaTwin_y) {
-  this.dna = createVector(dna_x,dna_y);
+Vehicle.prototype.behaviors = function (turn, sw, dna_x, dna_y, dnaTwin_x, dnaTwin_y, waveX, waveY, destroy, dt) {
+  this.dna = createVector(dna_x, dna_y);
   this.dnaTwin = createVector(dnaTwin_x, dnaTwin_y);
-  if(turn == 0){
-    if(sw == 0){
-    this.tar = this.dna;
-    }else{
+  if (turn == 1) {
+    if (destroy >= dt && destroy <= 1) {
+      this.tar = createVector(waveX, waveY);
+    } else {
+      this.tar = this.target;
+    }
+    // if(sw2 == 1){
+    // this.tar = createVector(waveX, waveY);
+    // }else{
+    //   this.tar = this.target;
+    // }
+    var flee_extra = createVector(random(width / 2.5, 2 * width / 2.5), random(0, height));
+    this.arrivemult = 0.5 * random();
+    this.fleemult = 1 * random();
+    this.maxforce = 1;
+    var flee_ex = this.flee(flee_extra);
+    flee_ex.mult(this.fleemult);
+    this.applyForce(flee_ex);
+  } else if (turn == 2) {
+    this.tar = this.dnaTwin;
+    this.arrivemult = 0.5 * random();
+    this.fleemult = 5 * random();
+    this.maxforce = 1;
+    var flee_extra = createVector(random(width / 2.5, 2 * width / 2.5), random(0, height));
+    var flee_ex = this.flee(flee_extra);
+    flee_ex.mult(this.fleemult);
+    this.applyForce(flee_ex);
+
+  } else if (turn == 3) {
+    if (sw == 0) {
+      this.tar = this.dna;
+    } else {
       this.tar = this.dnaTwin;
     }
-    this.arrivemult = 0.5*random();
-    this.fleemult = 5*random();
+    this.arrivemult = 0.3 * random();
+    this.fleemult = 1 * random();
     this.maxforce = 1;
-  }else{
+  } else {
     this.tar = this.target;
     this.arrivemult = 1;
     this.fleemult = 5;
@@ -31,32 +59,49 @@ Vehicle.prototype.behaviors = function(turn,sw,dna_x, dna_y, dnaTwin_x, dnaTwin_
   var arrive = this.arrive(this.tar);
   var mouse = createVector(mouseX, mouseY);
   var flee = this.flee(mouse);
+  flee.mult(this.fleemult);
+  this.applyForce(flee);
 
   arrive.mult(this.arrivemult);
-  flee.mult(this.fleemult);
-
   this.applyForce(arrive);
-  this.applyForce(flee);
 };
 
-Vehicle.prototype.applyForce = function(f) {
+Vehicle.prototype.applyForce = function (f) {
   this.acc.add(f);
 };
 
-Vehicle.prototype.update = function() {
+Vehicle.prototype.update = function () {
   this.pos.add(this.vel);
   this.vel.add(this.acc);
   this.acc.mult(0);
 };
 
-Vehicle.prototype.show = function(radius) {
+Vehicle.prototype.show = function (radius, n, sw) {
   this.radius = radius;
-  stroke(255);
-  strokeWeight(this.radius);
-  point(this.pos.x, this.pos.y);
+  if (n == 3) {
+    if (this.pos.y > 0 && this.pos.y < height && sw == 0) {
+      stroke(255, 255);
+      strokeWeight(this.radius / 3);
+      point(this.pos.x, this.pos.y);
+      stroke(255, 120);
+      strokeWeight(this.radius / 2);
+      point(this.pos.x, this.pos.y);
+      stroke(255, 50);
+      strokeWeight(this.radius);
+      point(this.pos.x, this.pos.y);
+    } else {
+      stroke(255);
+      strokeWeight(this.radius);
+      point(this.pos.x, this.pos.y);
+    }
+  } else {
+    stroke(255);
+    strokeWeight(this.radius);
+    point(this.pos.x, this.pos.y);
+  }
 };
 
-Vehicle.prototype.arrive = function(target) {
+Vehicle.prototype.arrive = function (target) {
   var desired = p5.Vector.sub(target, this.pos);
   var d = desired.mag();
   var speed = this.maxspeed;
@@ -69,10 +114,10 @@ Vehicle.prototype.arrive = function(target) {
   return steer;
 };
 
-Vehicle.prototype.flee = function(target) {
+Vehicle.prototype.flee = function (target) {
   var desired = p5.Vector.sub(target, this.pos);
   var d = desired.mag();
-  if (d < 50) {
+  if (d < 30) {
     desired.setMag(this.maxspeed);
     desired.mult(-1);
     var steer = p5.Vector.sub(desired, this.vel);
@@ -83,14 +128,14 @@ Vehicle.prototype.flee = function(target) {
   }
 };
 
-Vehicle.prototype.spin = function(counter) {
-  if(abs(this.pos.x - this.dna.x) <= counter && abs(this.pos.x - this.dnaTwin.x) <= counter){
-  stroke(255)
-  noFill();
-  strokeWeight(1);
-  beginShape();
-  vertex(this.dna.x, this.dna.y);
-  vertex(this.dnaTwin.x, this.dnaTwin.y);
-  endShape();
+Vehicle.prototype.spin = function (counter) {
+  if (abs(this.pos.x - this.dna.x) <= counter && abs(this.pos.x - this.dnaTwin.x) <= counter) {
+    stroke(255)
+    noFill();
+    strokeWeight(1);
+    beginShape();
+    vertex(this.dna.x, this.dna.y);
+    vertex(this.dnaTwin.x, this.dnaTwin.y);
+    endShape();
   }
 };
